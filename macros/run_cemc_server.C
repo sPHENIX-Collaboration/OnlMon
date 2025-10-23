f68390b0d75a938ded6ee710a0b5a9e623994bf4
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-10-23 12:55:46 -0400

    add setting of EventReceiverClient

--- macros/run_cemc_server.C
+++ macros/run_cemc_server.C
@@ -12,6 +12,10 @@ void run_cemc_server(const std::string &name = "CEMCMON", unsigned int serverid 
 {
   OnlMon *m = new CemcMon(name);                    // create subsystem Monitor object
   m->SetMonitorServerId(serverid);
+
+// If running the eventServer_classic on the local host for offline debugging
+// uncomment the m->SetEventReceiverClient("localhost");
+//  m->SetEventReceiverClient("localhost");
                                                 //  m->AddTrigger("PPG(Laser)");  // high efficiency triggers selection at et pool
                                                 //  m->AddTrigger("ONLMONBBCLL1"); // generic bbcll1 minbias trigger (defined in ServerFuncs.C)
   OnlMonServer *se = OnlMonServer::instance();  // get pointer to Server Framework
