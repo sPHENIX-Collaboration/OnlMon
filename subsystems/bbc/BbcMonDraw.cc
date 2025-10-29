0ff113bf7febd7fb13de8b22bc4f43ac094ad7ff
ahodges21 <ahodges4@illinois.edu>
2025-10-29 18:03:28 -0400

    Re-addition of status histograms because git ate them >:(

--- subsystems/bbc/BbcMonDraw.cc
+++ subsystems/bbc/BbcMonDraw.cc
@@ -23,6 +23,8 @@
 #include <TStyle.h>
 #include <TSystem.h>
 #include <TText.h>
+#include <THStack.h>
+#include <TLegend.h>
 
 #include <algorithm>
 #include <chrono>
@@ -1089,11 +1091,11 @@ int BbcMonDraw::MakeCanvas(const std::string &name)
     TC[7] = new TCanvas(name.c_str(), "Bbc Packet Information", -1, ysize, xsize , ysize);
     TC[7] -> Draw();
     gSystem->ProcessEvents();
-    Pad[2] = new TPad("cemcpad1", "packet event check", 0.0, 0.0, 0.78, 0.95, 0);
+    Pad[2] = new TPad("bbcpad1", "packet event check", 0.0, 0.0, 0.78, 0.95, 0);
     Pad[2]->SetLeftMargin(0.07);
     Pad[2]->SetRightMargin(0.05);
     Pad[2]->Draw();
-    Pad[3] = new TPad("cemcpad2", "packet event check legend", 0.75, 0.3, 0.95, 0.95, 0);
+    Pad[3] = new TPad("bbcpad2", "packet event check legend", 0.75, 0.3, 0.95, 0.95, 0);
     //Pad[2]->SetLeftMargin(0.05);
     Pad[3]->SetRightMargin(0);
     Pad[4] -> Draw();
@@ -1208,6 +1210,15 @@ int BbcMonDraw::Draw(const std::string &what)
     {
       canvasindex = 6;
       MakeCanvas("BbcMon5");
+    }
+  }
+  if (!gROOT->FindObject("BbcMon6"))
+  {
+    TC[7] = nullptr;
+     if (what == "BbcMon6" || what == "ALL")
+    {
+      canvasindex = 7;
+      MakeCanvas("BbcMon6");
     }
   }
   
@@ -1340,6 +1351,113 @@ int BbcMonDraw::Draw(const std::string &what)
     return 0;
   }
   
+  if (what == "BbcMon6")
+  {
+    OnlMonClient *cl = OnlMonClient::instance();
+    gStyle->SetOptStat(0);
+    
+    TH1 *packetStatusFull[nPacketStatus] = {nullptr};
+    int nServer = 0;
+    int colorsThatDontSuck[] = {kGreen+2,1,2,4, kViolet,kCyan,kOrange+2,kMagenta+2,kAzure-2};
+    int isAlert = false;
+    for (auto server = ServerBegin(); server != ServerEnd(); ++server)
+    {   
+      TH1 *packetStatus[nPacketStatus] = {nullptr};
+      for(int i = 0; i < nPacketStatus; i++) packetStatus[i] = cl->getHisto(*server, Form("h1_packet_status_%d",i));
+      
+      if(!packetStatus[0])continue;
+      for(int i = 0; i < nPacketStatus; i++)
+      {
+        if(((packetStatus[i] -> Integral()) && (i != 0)) || (isAlert == true))isAlert = true;
+        packetStatus[i] -> SetFillColor(colorsThatDontSuck[i]);
+        packetStatus[i]->SetLineColor(kBlack);
+        packetStatus[i]->SetLineWidth(1); // Optional: make lines thicker if needed
+      }
+
+      //Normalize
+      const int nBins = packetStatus[0] -> GetNbinsX();
+      float norm[nBins] = {0};
+      for(int i = 0; i < nBins; i++)
+      {
+        for(int j = 0; j < nPacketStatus; j++)
+        {
+          norm[i]+=packetStatus[j]->GetBinContent(i+1);
+        }
+      }
+      for(int i = 0; i < nPacketStatus; i++)
+      {
+        for(int j = 0; j < nBins; j++)
+        {
+          if(norm[j]>0)packetStatus[i]->SetBinContent(j+1, packetStatus[i]->GetBinContent(j+1)/norm[j]);
+          else packetStatus[i]->SetBinContent(j+1,0); 
+        }
+        nServer > 0 ? (TH1*)(packetStatusFull[i] -> Add(packetStatus[i])) : packetStatusFull[i] = packetStatus[i];
+      }
+      nServer++;
+    }
+
+    THStack *hs = new THStack("hs", "Event-Averaged Packet Status");
+    
+    TLegend *leg = new TLegend(0.05,0.1,0.95,1);
+    leg -> SetFillStyle(0);
+    leg -> SetTextSize(0.06);
+    //leg -> SetBorderSize(0);
+    std::string stati[nPacketStatus] = {"Good", "Malformed Packet Header", "Unknown Word Classifier", "Too Many FEMs in Packet", "Malformed FEM Header", "Wrong Number of FEMs"};
+
+    if(packetStatusFull[0])
+    {
+      for(int i = 0; i < nPacketStatus; i++)
+      {
+        hs->Add(packetStatusFull[i]);
+        leg->AddEntry(packetStatusFull[i],stati[i].c_str(),"f");
+      } 
+    }
+    else{
+      DrawDeadServer(transparent[canvasindex]);
+    }
+
+    if (!gROOT->FindObject("CemcMon2"))
+    {
+      MakeCanvas("CemcMon2");
+    }
+  
+    TC[canvasindex]->SetEditable(true);
+    TC[canvasindex]->Clear("D");
+    Pad[2]->cd(); 
+    if(hs)
+    {
+      hs->Draw("BAR");
+      hs->GetXaxis()->SetTitle("Packet Number");
+      hs->GetYaxis()->SetTitle("Event Fraction");
+      hs->GetYaxis()->SetTitleOffset(0.9);
+    }
+    Pad[3] -> cd();
+    leg->Draw();
+    TC[canvasindex]->Update();
+    TC[canvasindex]->Show();
+    packetWarning->cd();
+    TText warn;
+    warn.SetTextFont(62);
+    if(isAlert)
+    {
+      warn.SetTextSize(.15);
+      warn.SetTextColor(kRed);
+      warn.DrawText(0.05,0.5,"!WARNING!");
+      warn.DrawText(0.05,0.35,"POSSIBLE DATA CORRUPTION");
+      warn.DrawText(0.05,0.2,"CONTACT DAQ EXPERT");
+    }
+    else
+    {
+      warn.SetTextSize(0.3);
+      warn.DrawText(0.1,0.5,"All good!");
+    }
+    
+    TC[canvasindex]->SetEditable(false);
+
+    return 0;
+
+  }
+
   if (what == "BbcAutoUpdate" )
   {
     if (!gROOT->FindObject("BbcAutoUpdate"))
