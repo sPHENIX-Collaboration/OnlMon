b3084340f4ef0215bd4ee1f7cf44e84d45f9c337
mchiu-bnl <chiu@bnl.gov>
2025-12-10 09:40:44 -0500

    put spin and localpol into run_Poms.C

--- macros/run_Poms.C
+++ macros/run_Poms.C
@@ -245,26 +245,26 @@ void StartPoms()
   subsys->AddAction(new SubSystemActionSavePlot(subsys));
   pmf->RegisterSubSystem(subsys);
 
-  // subsys = new SubSystem("Itaru only", "spin");
-  // subsys->AddAction("spinDraw(\"FIRST\")", "Spin");
-  // subsys->AddAction("spinDraw(\"SECOND\")", "GL1p");
-  // subsys->AddAction("spinDraw(\"THIRD\")", "Abort Gap");
-  // subsys->AddAction(new SubSystemActionSavePlot(subsys));
-  // pmf->RegisterSubSystem(subsys);
+  subsys = new SubSystem("Spin", "spin");
+  subsys->AddAction("spinDraw(\"FIRST\")", "Spin");
+  subsys->AddAction("spinDraw(\"SECOND\")", "GL1p");
+  subsys->AddAction("spinDraw(\"THIRD\")", "Abort Gap");
+  subsys->AddAction(new SubSystemActionSavePlot(subsys));
+  pmf->RegisterSubSystem(subsys);
 
 
-  // subsys = new SubSystem("LOCALPOL", "localpol");
-  // subsys->AddAction("localpolDraw(\"FIRST\")", "Asymmetries [expert]");
-  // subsys->AddAction("localpolDraw(\"SECOND\")", "Polarisation direction [expert]");
-  // subsys->AddAction("localpolDraw(\"THIRD\")", "Spin pattern [expert]");
-  // subsys->AddAction("localpolDraw(\"FOURTH\")", "Trigger [expert]");
-  // subsys->AddAction("localpolDraw(\"FIFTH\")", "SMD [expert]");
-  // subsys->AddAction("localpolDraw(\"SIXTH\")", "waveforms [expert]");
-  // subsys->AddAction("localpolDraw(\"SEVENTH\")", "ZDC/GL1 matching [expert]");
-  // subsys->AddAction("localpolDraw(\"EIGHTH\")", "SMD 2D distributions [expert]");
-  // subsys->AddAction("localpolDraw(\"NINTH\")", "SMD 2D distributions [expert]");
-  // subsys->AddAction(new SubSystemActionSavePlot(subsys));
-  // pmf->RegisterSubSystem(subsys);
+  subsys = new SubSystem("LOCALPOL", "localpol");
+  subsys->AddAction("localpolDraw(\"FIRST\")", "Asymmetries [expert]");
+  subsys->AddAction("localpolDraw(\"SECOND\")", "Polarisation direction [expert]");
+  subsys->AddAction("localpolDraw(\"THIRD\")", "Spin pattern [expert]");
+  subsys->AddAction("localpolDraw(\"FOURTH\")", "Trigger [expert]");
+  subsys->AddAction("localpolDraw(\"FIFTH\")", "SMD [expert]");
+  subsys->AddAction("localpolDraw(\"SIXTH\")", "waveforms [expert]");
+  subsys->AddAction("localpolDraw(\"SEVENTH\")", "ZDC/GL1 matching [expert]");
+  subsys->AddAction("localpolDraw(\"EIGHTH\")", "SMD 2D distributions [expert]");
+  subsys->AddAction("localpolDraw(\"NINTH\")", "SMD 2D distributions [expert]");
+  subsys->AddAction(new SubSystemActionSavePlot(subsys));
+  pmf->RegisterSubSystem(subsys);
 
 
 
